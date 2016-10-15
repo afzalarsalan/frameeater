63c64a2fcca58971a352a1fbdfefbaa9a080e443
Arsalan Afzal <afzal.arsalan@gmail.com>
2016-10-15 23:29:03 +0000

    Git Ignore Images created

--- app.go
+++ app.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 )
 
 const (
-	DEFAULT_PORT = "8081"
+	DEFAULT_PORT = "9000"
 )
 
 var (
@@ -21,35 +22,11 @@ var (
 )
 
 func main() {
-	/*if port = os.Getenv("PORT"); len(port) == 0 {
-		port = DEFAULT_PORT
-	}
 	http.HandleFunc("/", homepage)
-	http.ListenAndServe(":"+port, nil)*/
-	for true {
-		cmd := exec.Command("ffmpeg", "-i", filename, "-vframes", "1", "-s", fmt.Sprintf("%dx%d", width, height), "-f", "singlejpeg", "-")
-		var buffer bytes.Buffer
-		cmd.Stdout = &buffer
-		err := cmd.Run()
-		if err != nil {
-			panic("could not get frame" + err.Error())
-		}
-		r := bytes.NewReader(buffer.Bytes())
-		//img, _ := jpeg.Decode(r)
-		file, err := os.Create("img.jpg")
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = io.Copy(file, r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		file.Close()
-		fmt.Println("Got Em")
-	}
+	http.ListenAndServe(":9000", nil)
 }
 
-/*func homepage(w http.ResponseWriter, req *http.Request) {
+func homepage(w http.ResponseWriter, req *http.Request) {
 	cmd := exec.Command("ffmpeg", "-i", filename, "-vframes", "1", "-s", fmt.Sprintf("%dx%d", width, height), "-f", "singlejpeg", "-")
 	var buffer bytes.Buffer
 	cmd.Stdout = &buffer
@@ -68,31 +45,5 @@ func main() {
 		log.Fatal(err)
 	}
 	file.Close()
-	fmt.Println("Got Em")
 	fmt.Fprintf(w, "we made it")
 }
-
-func imageScrapper() {
-	for true {
-		cmd := exec.Command("ffmpeg", "-i", filename, "-vframes", "1", "-s", fmt.Sprintf("%dx%d", width, height), "-f", "singlejpeg", "-")
-		var buffer bytes.Buffer
-		cmd.Stdout = &buffer
-		err := cmd.Run()
-		if err != nil {
-			panic("could not get frame" + err.Error())
-		}
-		r := bytes.NewReader(buffer.Bytes())
-		//img, _ := jpeg.Decode(r)
-		file, err := os.Create("img.jpg")
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = io.Copy(file, r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		file.Close()
-		fmt.Println("Got Em")
-	}
-}
-*/
